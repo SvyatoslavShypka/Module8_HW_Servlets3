97bf0566ae0ebfbcbf1f6b9399cd4bc29fc56c3d
Svyatoslav Shypka <svyat200909@gmail.com>
2023-06-03 17:50:40 +0200

    Almost ready2

--- src/main/java/com/goit/servlet/ApplicationInitServlet.java
+++ src/main/java/com/goit/servlet/ApplicationInitServlet.java
@@ -1,6 +1,5 @@
 package com.goit.servlet;
 
-import com.goit.conf.LoggingConfiguration;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.http.HttpServlet;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +9,6 @@ public class ApplicationInitServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) {
         log.info("Started configuration init");
-        new LoggingConfiguration().setup();
         log.info("Configuration init finished");
     }
 }
--- src/main/resources/db/migration/V2023.05.10.001__add-file-table.sql
+++
@@ -1,7 +0,0 @@
--- Create the file table
-CREATE TABLE app_file (
-    id VARCHAR(50) PRIMARY KEY,
-    name VARCHAR(255),
-    extension VARCHAR(50),
-    creation_date BIGINT
-);
